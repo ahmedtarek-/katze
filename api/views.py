819ef3255bf8ede903b9eca1334372ad441f383e
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-10-31 05:09:55 +0100

    [views] calls call_external_cats_api method from view to fetch images then return it as response

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .helpers import call_external_cats_api
+
+
+class CatApiView(APIView):
+    """
+    API for retrieving cats
+    """
+
+    def get(self, request, format=None):
+        tag = request.GET.get('tag', None)
+        images_paths = call_external_cats_api()
+
+        data = list(map(lambda x: {'img': x}, images_paths))
+        return Response(data, status=status.HTTP_200_OK)
