d161686de189ef2fbb46011c131da98e216c9545
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-11-02 15:16:30 +0100

    [cache] defines cache in separate module that uses django database cache and filesystem

---
+++ django_cache/cache.py
@@ -0,0 +1,46 @@
+from django.core.files.storage import FileSystemStorage
+from django.contrib.sites.models import Site
+from django.core.cache import cache
+
+
+class Cache:
+    """
+    Uses File based caching using django filesytemstorage
+    +
+    Uses django database cache (key/value) to store tag (key) and images
+    paths (values)
+    """
+    DEFAULT_IMG_EXTENSION = 'png'
+    DOMAIN_BASE_URL = Site.objects.get_current().domain
+
+    def get(self, key):
+        return cache.get(key)
+
+    def set(self, key, images):
+        paths = self._save_images(key, images)
+        cache.set(key, paths)
+
+        return paths
+
+    def _save_images(self, key, images):
+        """saves images in file system and returns paths"""
+
+        paths = []
+        for i, image in enumerate(images):
+            image_name = self._construct_image_name(key, i)
+            image_path = self._save_image(image, image_name)
+            paths.append(image_path)
+
+        return paths
+
+    def _save_image(self, image, image_name):
+        fs = FileSystemStorage()
+
+        fs.save(image_name, image)
+        path = ''.join([self.DOMAIN_BASE_URL, fs.url(image_name)])
+
+        return path
+
+
+    def _construct_image_name(self, tag, number):
+        return f"{tag}_{number}.{self.DEFAULT_IMG_EXTENSION}"
