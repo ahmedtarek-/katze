9cbea38ea75a7c6eaedbd9179f534ed89502cd7b
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-11-02 15:15:52 +0100

    [urls] adds media_root to url_patterns + pass tag as url param instead of GET param

--- katze/urls.py
+++ katze/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 
 from api import views
 
 urlpatterns = [
-    path('cats/', views.CatApiView.as_view(), name='cats-api'),
+    path('cats/<tag>/', views.CatApiView.as_view(), name='cats-api'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
